2e5ed26d141fe7bba7587da323414587d5665225
Sota Tanaka <tennisboy_s@me.com>
2022-03-01 11:16:17 +0900

    view file added

---
+++ src/ofxDrawJSON.cpp
@@ -0,0 +1,8 @@
+//
+//  ofxDrawJson.cpp
+//  ISCA2021
+//
+//  Created by Sota Tanaka on 2022/03/01.
+//
+
+#include "ofxDrawJSON.h"
---
+++ src/ofxDrawJSON.h
@@ -0,0 +1,13 @@
+//
+//  ofxDrawJson.hpp
+//  ISCA2021
+//
+//  Created by Sota Tanaka on 2022/03/01.
+//
+
+#ifndef ofxDrawJSON_h
+#define ofxDrawJSON_h
+
+#include <stdio.h>
+
+#endif /* ofxDrawJson_h */
